ac90f56aeed88749fb4ad3e3aff409cd1cf4c8b4
ShivrajJ <shivraj.jodha@outlook.com>
2023-10-26 20:14:41 +0530

    Added Active Red Bar for easier Visualization

--- visualizing-sorting/src/App.tsx
+++ visualizing-sorting/src/App.tsx
@@ -8,25 +8,25 @@ function swap(arr:Array<number>, a:number, b:number):void {
 }
 const timer = (ms:number) => new Promise(res => setTimeout(res, ms))
 
-async function bubbleSort(arr : Array<number>, setArr : React.Dispatch<React.SetStateAction<number[]>>): Promise<Array<number>> {
+async function bubbleSort(arr : Array<number>, setArr: React.Dispatch<React.SetStateAction<number[]>>, setActive: React.Dispatch<React.SetStateAction<number>>): Promise<Array<number>> {
   var flag:boolean = true;
-  var iteration:number = 0;
   while(flag) {
     flag = false;
     for (let i : number = 0; i < arr.length - 1; i++) {
       if(arr[i] < arr[i+1]) {
-        iteration = iteration+1;
         swap(arr, i, i+1);
+        setActive(i);
         flag = true;
         await timer(20);
         setArr([...arr]);
       }
     }
   }
+  setActive(-1);
   return arr;
 }
 
-async function selectionSort(arr : Array<number>, setArr : React.Dispatch<React.SetStateAction<number[]>>): Promise<Array<number>> {
+async function selectionSort(arr : Array<number>, setArr: React.Dispatch<React.SetStateAction<number[]>>, setActive: React.Dispatch<React.SetStateAction<number>>): Promise<Array<number>> {
   for(let i : number = 0; i<arr.length-1; i++) {
     var biggest:number = i;
     for(let j = i+1; j < arr.length; j++) {
@@ -35,25 +35,29 @@ async function selectionSort(arr : Array<number>, setArr : React.Dispatch<React.
         await timer(20);
       }
     }
+    setActive(biggest);
     swap(arr, i, biggest);
     // await timer(20);
     setArr([...arr]);
   }
+  setActive(-1);
   return arr;
 }
-async function insertionSort(arr:Array<number>, setArr: React.Dispatch<React.SetStateAction<number[]>>): Promise<Array<number>> {
+async function insertionSort(arr:Array<number>, setArr: React.Dispatch<React.SetStateAction<number[]>>, setActive: React.Dispatch<React.SetStateAction<number>>): Promise<Array<number>> {
   for(let i:number = 1; i < arr.length; i++) {
     var temp:number = i;
     while(temp>0 && arr[temp] > arr[temp-1]) {
       swap(arr, temp, temp-1);
+      setActive(temp)
       temp--;
       await timer(20);
       setArr([...arr]);
     }
   }
+  setActive(-1);
   return arr;
 }
-async function mergeSort(arr : Array<number>, setArr : React.Dispatch<React.SetStateAction<number[]>>): Promise<Array<number>> {
+async function mergeSort(arr : Array<number>, setArr: React.Dispatch<React.SetStateAction<number[]>>, setActive: React.Dispatch<React.SetStateAction<number>>): Promise<Array<number>> {
   async function merge(arr:Array<number>, l:number, mid:number, r:number): Promise<void> {
     var n1:number = mid-l+1;
     var n2:number = r-mid;
@@ -100,14 +104,17 @@ async function mergeSort(arr : Array<number>, setArr : React.Dispatch<React.SetS
     var mid:number = Math.floor((l+r) / 2);
     await mSort(arr, l, mid);
     await mSort(arr, mid+1, r);
+    setActive(mid);
     await merge(arr, l, mid, r);
   }
   await mSort(arr, 0, arr.length-1);
+  setActive(-1);
   return arr;
 }
-async function quickSort(arr : Array<number>, setArr : React.Dispatch<React.SetStateAction<number[]>>): Promise<Array<number>> {
+async function quickSort(arr : Array<number>, setArr: React.Dispatch<React.SetStateAction<number[]>>, setActive: React.Dispatch<React.SetStateAction<number>>): Promise<Array<number>> {
   async function partition(arr:Array<number>, low:number, high:number): Promise<number> {
     var pivot = arr[low];
+    setActive(low);
     var j:number = high;
     for(var i = high; i > low; i--) {
       if(arr[i] < pivot) {
@@ -118,6 +125,7 @@ async function quickSort(arr : Array<number>, setArr : React.Dispatch<React.SetS
       }
     }
     swap(arr, low, j);
+    setActive(j);
     await timer(20);
     setArr([...arr]);
     return j;
@@ -130,6 +138,7 @@ async function quickSort(arr : Array<number>, setArr : React.Dispatch<React.SetS
     }
   }
   await qSort(arr, 0, arr.length-1);
+  setActive(-1);
   return arr;
 }
 
--- visualizing-sorting/src/components/Sort.tsx
+++ visualizing-sorting/src/components/Sort.tsx
@@ -3,16 +3,17 @@ import './Sort.css'
 
 interface SortFunction {
     sortName: string;
-    sortFunc: (arr:Array<number>, setArray: React.Dispatch<React.SetStateAction<number[]>>) => Promise<Array<number>>
+    sortFunc: (arr:Array<number>, setArray: React.Dispatch<React.SetStateAction<number[]>>, setActive: React.Dispatch<React.SetStateAction<number>>) => Promise<Array<number>>
 }
 
 export default function Sort({sortName, sortFunc} : SortFunction) {
     const [array, setArray] = useState<Array<number>>(Array.from({length:100}, () => Math.floor(Math.random()*100)));
+    const [active, setActive] = useState<number>(0);
     function resetArr() : void {
         setArray(Array.from({length:100}, () => Math.floor(Math.random()*100)));
     }
     function startSort() : void {
-        sortFunc([...array], setArray);
+        sortFunc([...array], setArray, setActive);
     }
     useEffect(()=> {
         console.log("update");
@@ -23,9 +24,9 @@ export default function Sort({sortName, sortFunc} : SortFunction) {
             <button onClick={startSort}>Begin</button>
             <button onClick={resetArr}>Reset</button>
             <div className='sort-div'>
-                {array.map((num) => {
+                {array.map((num, idx) => {
                     return(
-                        <span className='sort-bar' style={{height:num*5}}></span>
+                        <span className={`sort-bar ${idx==active?'active-sort-bar':''}`} style={{height:num*5}}></span>
                     ) 
                 })}
             </div>
